26058bea28be0933a85b399b651e2c0608eb4cd6
zsmeikle <zmeikle@tamu.edu>
2024-09-30 19:47:56 -0500

    Code update
    Mouse movement and actions should be done

--- Input_Mapping.py
+++ Input_Mapping.py
@@ -7,12 +7,17 @@ Scale = 1                                                          # scales the 
 Switch_XY = 0                                                      # switches X with Y and vice versa (0 = off, 1 = on)
 Reverse_X = 1                                                      # reverses X (1 = off, -1 = on)
 Reverse_Y = 1                                                      # reverses Y (1 = off, -1 = on)
+Refresh_Rate = 20                                                  # refreshrate of the board/data
                                                                    #
 #Code______________________________________________________________#
 mouse = Controller()
+Delay = 1/Refresh_Rate
 
 tempX = 1 #replace when Matlab
 tempY = 1 #replace when Matlab
+tempRight_Click = 0 #replace when Matlab
+tempLeft_Click = 0 #replace when Matlab
+
 X = 0
 Y = 0
 
@@ -22,13 +27,24 @@ while True:
         case 0:
            X = Scale * Reverse_X * tempX
            Y = Scale * Reverse_Y * tempY 
-        case 0:
+        case 1:
            X = Scale * Reverse_X * tempY
            Y = Scale * Reverse_Y * tempX
     
-    mouse.move(x[n%4], y[n%4])
-    n += 1
-    time.sleep(0.05)
+    match tempLeft_Click:
+        case 0:
+            mouse.release(Button.left)
+        case 1:
+            mouse.press(Button.left)
+
+    match tempRight_Click:
+        case 0:
+            mouse.release(Button.right)
+        case 1:
+            mouse.press(Button.right)
+    
+    mouse.move(X, Y)
+    time.sleep(Delay)
     
     
 
