65f5e2b5eaf027cb7225d93c2c1111fd6b0c6cfc
zsmeikle <zmeikle@tamu.edu>
2024-09-30 20:04:00 -0500

    Added some comments

--- Input_Mapping.py
+++ Input_Mapping.py
@@ -10,41 +10,41 @@ Reverse_Y = 1                                                      # reverses Y 
 Refresh_Rate = 20                                                  # refreshrate of the board/data
                                                                    #
 #Code______________________________________________________________#
-mouse = Controller()
-Delay = 1/Refresh_Rate
-
-tempX = 1 #replace when Matlab
-tempY = 1 #replace when Matlab
-tempRight_Click = 0 #replace when Matlab
-tempLeft_Click = 0 #replace when Matlab
-
-X = 0
-Y = 0
-
-
-while True:
-    match Switch_XY:
-        case 0:
-           X = Scale * Reverse_X * tempX
-           Y = Scale * Reverse_Y * tempY 
-        case 1:
-           X = Scale * Reverse_X * tempY
-           Y = Scale * Reverse_Y * tempX
-    
-    match tempLeft_Click:
-        case 0:
-            mouse.release(Button.left)
-        case 1:
-            mouse.press(Button.left)
-
-    match tempRight_Click:
-        case 0:
-            mouse.release(Button.right)
-        case 1:
-            mouse.press(Button.right)
-    
-    mouse.move(X, Y)
-    time.sleep(Delay)
+mouse = Controller()                                               # defines mouse so we can control it
+Delay = 1/Refresh_Rate                                             # Coverts to delay fro mrefresh rate
+                                                                   #
+tempX = 1 #replace when Matlab                                     # these variable will come from Matlab later
+tempY = 1 #replace when Matlab                                     #
+tempRight_Click = 0 #replace when Matlab                           #
+tempLeft_Click = 0 #replace when Matlab                            #
+                                                                   #
+X = 0                                                              # Initialize X and Y
+Y = 0                                                              #
+                                                                   #
+                                                                   #
+while True: #maybe add kill variable                               # Loop to keep running
+    match Switch_XY:                                               # If we are switching X and Y
+        case 0:                                                    #
+           X = Scale * Reverse_X * tempX                           #
+           Y = Scale * Reverse_Y * tempY                           #
+        case 1:                                                    #
+           X = Scale * Reverse_X * tempY                           #
+           Y = Scale * Reverse_Y * tempX                           #
+                                                                   #
+    match tempLeft_Click:                                          # If left click
+        case 0:                                                    # Posibly inefficient but we can worry about that later
+            mouse.release(Button.left)                             #
+        case 1:                                                    #
+            mouse.press(Button.left)                               #
+                                                                   #
+    match tempRight_Click:                                         # If right click
+        case 0:                                                    # See prev comment
+            mouse.release(Button.right)                            #
+        case 1:                                                    #
+            mouse.press(Button.right)                              #
+                                                                   #
+    mouse.move(X, Y)                                               # Actually impliment the mouse movement
+    time.sleep(Delay)                                              # Delay as per refresh rate
     
     
 
