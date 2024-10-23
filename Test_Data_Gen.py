118c941afd2bc7184d5410d190e3d2c21ca0aadf
zsmeikle <zmeikle@tamu.edu>
2024-10-23 10:59:01 -0500

    Updated test data

--- Test_Data_Gen.py
+++ Test_Data_Gen.py
@@ -1,8 +1,31 @@
 
 
 with open("Test_Data.csv", "w") as Test_Data:
-    tempX = 0 
-    tempY = 1
-    tempRight_Click = 2
-    tempLeft_Click = 3
-    Test_Data.write(str(tempX) + " " + str(tempY) + " " + str(tempRight_Click) + " " + str(tempLeft_Click) + "\n")
+    tempX = -10 
+    tempY = -10
+    tempRight_Click = 0
+    tempLeft_Click = 0
+    x = 0
+    while x < 1800:
+        x += 1
+        Test_Data.write(str(tempX) + " " + str(tempY) + " " + str(tempRight_Click) + " " + str(tempLeft_Click) + "\n")
+        if (x == 400):
+            tempX = 10
+            tempY = 0
+        elif (x == 800):
+            tempX = 0
+            tempY = 10
+        elif (x == 1200):
+            tempX = -10
+            tempY = 0
+        elif (x == 1600):
+            tempX = 0
+            tempY = 0
+            tempLeft_Click = 1
+        elif (x == 1650):
+            tempLeft_Click = 0
+        elif (x == 1700):
+            tempRight_Click = 1
+        elif (x == 1750):
+            tempRight_Click = 0
+        
