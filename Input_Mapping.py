d876c85b4a5370002929795560b778551ba5d8c7
zsmeikle <zmeikle@tamu.edu>
2024-10-28 18:09:40 -0500

    Saving progress
    working on implementing test code into normal function.

--- Input_Mapping.py
+++ Input_Mapping.py
@@ -1,6 +1,7 @@
 #Imports___________________________________________________________# 
 from pynput.mouse import Button, Controller                        # Allows us to control mouse
 import time                                                        # Allows us to add delays 
+import csv                                                         # Allows us to read csv files (for testing)
                                                                    #
 #Settings__________________________________________________________# These variables let us change different factors
 Scale = 1                                                          # scales the X and Y movements by the value
@@ -8,21 +9,56 @@ Switch_XY = 0                                                      # switches X 
 Reverse_X = 1                                                      # reverses X (1 = off, -1 = on)
 Reverse_Y = 1                                                      # reverses Y (1 = off, -1 = on)
 Refresh_Rate = 20                                                  # refreshrate of the board/data
+Test_File = "Test_Data.csv"                                        # the test file being used to demo
+                                                                   #
+#Test_Functions____________________________________________________# This section is only for testing
+def get_data(line, size):                                          # get a specific line of data from the test file
+    result = -1                                                    # if output is -1 it is eof or error
+    with open(Test_File, "r") as Test_Data:                        #
+        data = csv.reader(Test_Data)                               #
+        row = list(data)                                           #
+        if (line < size):                                          #
+            result = row[line]                                     #
+    return result                                                  #
+                                                                   #
+def get_size():                                                    # get the size of a csv file
+    result = 0                                                     #
+    with open(Test_File, "r") as Test_Data:                        #
+        data = csv.reader(Test_Data)                               #
+        for i, row in enumerate(data):                             #
+            result += 1                                            #
+    return result                                                  #
                                                                    #
 #Code______________________________________________________________#
 mouse = Controller()                                               # defines mouse so we can control it
 Delay = 1/Refresh_Rate                                             # Coverts to delay fro mrefresh rate
+Frame_Num = 0 #TESTING ONLY, REMOVE LATER!!!                       # Test variable for frame number
                                                                    #
-tempX = 1 #replace when Matlab                                     # these variable will come from Matlab later
-tempY = 1 #replace when Matlab                                     #
-tempRight_Click = 0 #replace when Matlab                           #
-tempLeft_Click = 0 #replace when Matlab                            #
+tempX = 1                                                          # these variable will come from Matlab later
+tempY = 1                                                          #
+tempRight_Click = 0                                                #
+tempLeft_Click = 0                                                 #
                                                                    #
 X = 0                                                              # Initialize X and Y
 Y = 0                                                              #
                                                                    #
                                                                    #
 while True: #maybe add kill variable                               # Loop to keep running
+                                                                   #
+    #TESTING_CODE--------------------#                             # Test code
+    size = get_size()                #                             # get the size of the file for get_data function
+    test = get_data(Frame_Num, size) #                             # Gets the data at the frame number
+    if (test == -1):                 #                             # if at eof
+        print("end of file")         #                             #
+        break                        #                             #
+    tempX = test[0]                  #                             # define variables from data
+    tempY = test[1]                  #                             #
+    tempRight_Click = test[2]        #                             #
+    tempLeft_Click = test[3]         #                             #
+    Frame_Num += 1                   #                             # incriment frame number
+    #END_OF_TESTING CODE-------------#                             #
+                                                                   #
+                                                                   #
     match Switch_XY:                                               # If we are switching X and Y
         case 0:                                                    #
            X = Scale * Reverse_X * tempX                           #
