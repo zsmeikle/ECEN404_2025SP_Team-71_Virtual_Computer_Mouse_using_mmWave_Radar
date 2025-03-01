7571c46d1f71b2fdec2c5616b9554c9c66e8d3cb
KAY2ZEN <149115339+KAY2ZEN@users.noreply.github.com>
2025-02-28 20:06:05 -0600

    Add files via upload

---
+++ radar_filter.py
@@ -0,0 +1,34 @@
+#pointCloud is an array of point cloud data
+#pointCloud contains at least 1 point
+#y is distance and always positive
+def filter_ys(pointCloud):
+    newPointCloud = []
+    y_avg = 0
+    for point in pointCloud:
+        y_avg += point[1]
+    y_avg /= len(pointCloud)
+    print(y_avg)
+    for point in pointCloud:
+        minY = .8*y_avg
+        maxY = 1.2*y_avg
+        if point[1] > minY and point[1] < maxY:
+            newPointCloud.append(point)
+    return newPointCloud
+
+def simple_avg(vals):
+
+    avg_x = 0
+    for x in vals:
+        avg_x += x
+    avg_x /= len(vals)
+    return avg_x
+
+def exponetial_avg(vals):
+    avg_x = 0
+    total_weights = 0
+    for x in range(len(vals)):
+        total_weights += x+1
+        avg_x += vals[x]*(x+1)
+    avg_x /= total_weights
+    return avg_x
+    
