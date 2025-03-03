95d43a0089f35ca1a6177e2b4dda6b4312bba147
zsmeikle <zmeikle@tamu.edu>
2025-03-03 08:39:29 -0600

    pushing updates

--- Input_Mapping.py
+++ Input_Mapping.py
@@ -3,7 +3,7 @@ from pynput.mouse import Button, Controller                        # Allows us t
 import time                                                        # Allows us to add delays 
 import csv                                                         # Allows us to read csv files (for testing)
 import threading                                                   # Allows us to make threads
-import subprocess                                                  #
+#import subprocess                                                  #
 import queue                                                       #
                                                                    #
 #Settings__________________________________________________________# These variables let us change different factors
@@ -38,8 +38,8 @@ def run_frame_gen(shared_queue):
     not_kill = True
     frames = 5
     while not_kill:
+        t0 
         try:
-        #if(not shared_queue.empty()):
             X, Y, frames, not_kill= shared_queue.get(timeout=1/22) 
              
             for  i in range(0, frames, 1):
@@ -49,111 +49,112 @@ def run_frame_gen(shared_queue):
             pass
                                                                    #
 #Code______________________________________________________________#
-def main():
-    shared_queue = queue.Queue()
-    mouse = Controller()                                               # defines mouse so we can control it
-    Frame_Num = 0 #TESTING ONLY, REMOVE LATER!!!                       # Test variable for frame number
+#def main():
+shared_queue = queue.Queue()
+mouse = Controller()                                               # defines mouse so we can control it
+Frame_Num = 0 #TESTING ONLY, REMOVE LATER!!!                       # Test variable for frame number
                                                                     #
-    No_Error = True                                                    # Kill variable for loop if error
+No_Error = True                                                    # Kill variable for loop if error
                                                                     #
-    if (Refresh_Rate <= 0):                                            # Check if refresh rate is within bounds
-        print('ERROR: Refresh_Rate must be greater than 0.')           #
-        No_Error = False                                               #
-    else:                                                              #
-        Delay = 1/Refresh_Rate                                         # Coverts to delay from refresh rate
+if (Refresh_Rate <= 0):                                            # Check if refresh rate is within bounds
+    print('ERROR: Refresh_Rate must be greater than 0.')           #
+    No_Error = False                                               #
+else:                                                              #
+    Delay = 1/Refresh_Rate                                         # Coverts to delay from refresh rate
                                                                     #
-    if ((Reverse_X != 1) & (Reverse_X != -1)):                         # Check Reverse_X is -1 or 1
-        print('ERROR: Reverse_X must be 1 or -1.')                     #
-        No_Error = False                                               #
-    if ((Reverse_Y != 1) & (Reverse_Y != -1)):                         # Check Reverse_Y is -1 or 1
-        print('ERROR: Reverse_Y must be 1 or -1.')                     #
-        No_Error = False                                               #
-    if ((Switch_XY != 1) & (Switch_XY != 0)):                          # Check Switch-XY is 0 or 1
-        print('ERROR: Switch_XY must be 0 or 1.')                      #
-        No_Error = False                                               #
-    if (isinstance(Scale, int) != True):                               # Check Scale is an int
-        print('ERROR: Scale must be an int.')                          #
-        No_Error = False                                               #
+if ((Reverse_X != 1) & (Reverse_X != -1)):                         # Check Reverse_X is -1 or 1
+    print('ERROR: Reverse_X must be 1 or -1.')                     #
+    No_Error = False                                               #
+if ((Reverse_Y != 1) & (Reverse_Y != -1)):                         # Check Reverse_Y is -1 or 1
+    print('ERROR: Reverse_Y must be 1 or -1.')                     #
+    No_Error = False                                               #
+if ((Switch_XY != 1) & (Switch_XY != 0)):                          # Check Switch-XY is 0 or 1
+    print('ERROR: Switch_XY must be 0 or 1.')                      #
+    No_Error = False                                               #
+if (isinstance(Scale, int) != True):                               # Check Scale is an int
+    print('ERROR: Scale must be an int.')                          #
+    No_Error = False                                               #
+                                                                #
+                                                                    #
+tempX = 1                                                          # these variable are temporary
+tempY = 1                                                          #
+tempRight_Click = 0                                                #
+tempLeft_Click = 0                                                 #
+                                                                    #
+X = 0                                                              # Initialize X and Y
+Y = 0                                                              #
+t0 = 0                                                             # initial time variable
+                                                                    #
+size = get_size()#Testing only                                     # get the size of the file for get_data function
                                                                     #
                                                                     #
-    tempX = 1                                                          # these variable are temporary
-    tempY = 1                                                          #
-    tempRight_Click = 0                                                #
-    tempLeft_Click = 0                                                 #
-                                                                    #
-    X = 0                                                              # Initialize X and Y
-    Y = 0                                                              #
-    t0 = 0                                                             # initial time variable
-                                                                    #
-    size = get_size()#Testing only                                     # get the size of the file for get_data function
-                                                                    #
-                                                                    #
-    thread = threading.Thread(target=run_frame_gen, args=(shared_queue,))
-    thread.start()
+thread = threading.Thread(target=run_frame_gen, args=(shared_queue,))
+thread.start()
     
-    while No_Error:                                                    # Loop to keep running
-        t0 = time.time()                                               # get initial time
+while No_Error:                                                    # Loop to keep running
+    t0 = time.time()                                               # get initial time
                                                                     #
-        #TESTING_CODE--------------------#                             # Test code
-        size = get_size()                #                             # get the size of the file for get_data function
-        test = get_data(Frame_Num, size) #                             # Gets the data at the frame number
-        if (test == -1):                 #                             # if at eof end loop
-            print("end of file")         #                             #
-            break                        #                             #
-        tempX = int(test[0])             #                             # define variables from data
-        tempY = int(test[1])             #                             #
-        tempRight_Click = int(test[2])   #                             #
-        tempLeft_Click = int(test[3])    #                             #
-        Frame_Num += 1                   #                             # incriment frame number
-        #END_OF_TESTING CODE-------------#                             #
+    #TESTING_CODE--------------------#                             # Test code
+    size = get_size()                #                             # get the size of the file for get_data function
+    test = get_data(Frame_Num, size) #                             # Gets the data at the frame number
+    if (test == -1):                 #                             # if at eof end loop
+        print("end of file")         #                             #
+        break                        #                             #
+    tempX = int(test[0])             #                             # define variables from data
+    tempY = int(test[1])             #                             #
+    tempRight_Click = int(test[2])   #                             #
+    tempLeft_Click = int(test[3])    #                             #
+    Frame_Num += 1                   #                             # incriment frame number
+    #END_OF_TESTING CODE-------------#                             #
                                                                     #
-        match Switch_XY:                                               # If we are switching X and Y
-            case 0:                                                    #
-                X = Scale * Reverse_X * tempX                           #
-                Y = Scale * Reverse_Y * tempY                           #
-            case 1:                                                    #
-                X = Scale * Reverse_X * tempY                           #
-                Y = Scale * Reverse_Y * tempX                           #
-            case default:                                              # Invalid Switch_XY
-                print("ERROR: Switch-XY must be a 0 or a 1.")          #
-                break                                                  #
+    match Switch_XY:                                               # If we are switching X and Y
+        case 0:                                                    #
+            X = Scale * Reverse_X * tempX                           #
+            Y = Scale * Reverse_Y * tempY                           #
+        case 1:                                                    #
+            X = Scale * Reverse_X * tempY                           #
+            Y = Scale * Reverse_Y * tempX                           #
+        case default:                                              # Invalid Switch_XY
+            print("ERROR: Switch-XY must be a 0 or a 1.")          #
+            break                                                  #
                                                                     #
-        match tempLeft_Click:                                          # If left click
-            case 0:                                                    # if nothing no-op
-                True                                                   #
-            case 1:                                                    #
-                mouse.release(Button.left)                             #
-            case 2:                                                    #
-                mouse.press(Button.left)                               #
-            case default:                                              # Error invaild input
-                print("ERROR: invalid left click input: " + str(tempLeft_Click))
-                break                                                  #
-        match tempRight_Click:                                         # If right click
-            case 0:                                                    # if nothing no-op
-                True                                                   #
-            case 1:                                                    #
-                mouse.release(Button.right)                            #
-            case 2:                                                    #
-                mouse.press(Button.right)                              #
-            case default:                                              # Error invaild input
-                print("ERROR: invalid right click input: " + str(tempRight_Click))
-                break                                                  #
+    match tempLeft_Click:                                          # If left click
+        case 0:                                                    # if nothing no-op
+            True                                                   #
+        case 1:                                                    #
+            mouse.release(Button.left)                             #
+        case 2:                                                    #
+            mouse.press(Button.left)                               #
+        case default:                                              # Error invaild input
+            print("ERROR: invalid left click input: " + str(tempLeft_Click))
+            break                                                  #
+    match tempRight_Click:                                         # If right click
+        case 0:                                                    # if nothing no-op
+            True                                                   #
+        case 1:                                                    #
+            mouse.release(Button.right)                            #
+        case 2:                                                    #
+            mouse.press(Button.right)                              #
+        case default:                                              # Error invaild input
+            print("ERROR: invalid right click input: " + str(tempRight_Click))
+            break                                                  #
                                                                     #
-        shared_queue.put((X, Y, 5, True))
-        #mouse.move(X, Y)                                               # Actually impliment the mouse movement
-        New_Delay = Delay-(time.time() - t0)                           # Calculate delay with processing time
-        #print(New_Delay)
-        if (New_Delay < 0):                                            # Skip a frame if took to long to compute
-            time.sleep(New_Delay + Delay)                              #
-            print("Skipped Frame: " + str(Frame_Num))                  # Print if a frame was skipped
-        else:                                                          #
-            time.sleep(New_Delay)                                      # Delay as per refresh rate and time code took
-    thread.join()
-    shared_queue.put((0, 0, 0, False))
-    thread.stop()
+    shared_queue.put((X, Y, 5, True))
+    #mouse.move(X, Y)                                               # Actually impliment the mouse movement
+    New_Delay = Delay-(time.time() - t0)                           # Calculate delay with processing time
+    #print(New_Delay)
+    if (New_Delay < 0):                                            # Skip a frame if took to long to compute
+        time.sleep(New_Delay + Delay)                              #
+        print("Skipped Frame: " + str(Frame_Num))                  # Print if a frame was skipped
+    else:                                                          #
+        time.sleep(New_Delay)                                      # Delay as per refresh rate and time code took
+
+shared_queue.put((0, 0, 0, False))
+thread.join()
+#thread.stop()
     #__________________________________________________________________#
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
 
 
 
